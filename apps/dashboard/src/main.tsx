19a4ca5d4a8154e8dbbf2e7cc0561f1eea3910b1
Adekola Adediwura <adekolaadediwura@gmail.com>
2025-11-18 23:22:21 +0100

    Auth form adjust

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -4,10 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  createRoot(rootElement).render(
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  );
+} else {
+  throw new Error('Root element with id "root" not found');
+}
