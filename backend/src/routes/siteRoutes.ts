9807fda8244f26c13fe0d0e615a2939b5b542fdf
Adekola Adediwura <adekolaadediwura@gmail.com>
2025-11-19 15:02:40 +0100

    add auth middleware to a site route

--- backend/src/routes/siteRoutes.ts
+++ backend/src/routes/siteRoutes.ts
@@ -7,7 +7,7 @@ import {
 } from "../controllers/siteController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
-router.get("/all", getSiteIds);
+router.get("/all", protect, getSiteIds);
 router.post("/add", protect, addNewSite);
 router.get("/:sideId", validateSiteId);
 
